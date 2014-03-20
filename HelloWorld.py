faaa973b3e42376daa1c8017757f5dea05520180
Andreas Friedl <s1310629009@students.fh-hagenberg.at>
2014-03-20 17:11:54 +0100

    pyglet simple window

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,17 @@
 __author__ = 'win8'
-print("Hello MegaAwesome World!")
+
+import pyglet
+
+print("Be aware that pyglet requires 32bit version of python!")
+
+window = pyglet.window.Window()
+
+label = pyglet.text.Label('Hello world!',font_name='Times New Roman',font_size=36,x=window.width//2, y=window.height//2,anchor_x='center', anchor_y='center')
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+pyglet.app.run()
+

f109ad9d466cfe5d2c8b66a172ae0c4e8a2bdb46
cassiss <sarah.troschl@yahoo.de>
2014-03-24 14:43:55 +0100

    load sprite, exchange sprite, animate sprite (loop) bad code but works

--- HelloWorld.py
+++ HelloWorld.py
@@ -16,13 +16,51 @@ label = pyglet.text.Label('Hello world!', font_name='Times New Roman', font_size
                           y=window.height // 2, anchor_x='center', anchor_y='center')
 
 """ use any picture """
-image  = pyglet.resource.image("small_cat.jpg")
+image = pyglet.resource.image("small_cat.jpg")
+
+starLeft = pyglet.resource.image("walk_left.png")
+starRight = pyglet.resource.image("walk_right.png")
+starLeftEvent= pyglet.resource.image("catch_left.png")
+starRightEvent = pyglet.resource.image("catch_right.png")
+
+spritePositionX = window.width / 2
+spritePositionY = window.height / 2
+
+starSprite = pyglet.sprite.Sprite(starLeft, spritePositionX, spritePositionY)
+lookLeft = 1
+
+danceAnimation = pyglet.image.Animation.from_image_sequence\
+    ([starLeft, starLeftEvent, starRight, starRightEvent], 0.5, True)
 
 @window.event()
 def on_key_press(symbol, modifiers):
     print "a key was pressed"
-    if symbol== key.LEFT:
+    if symbol == key.LEFT:
         print "left key was pressed"
+        starSprite.x -= 5
+        if lookLeft != 1:
+            global lookLeft
+            lookLeft = 1
+            starSprite.image = starLeft
+
+
+    if symbol == key.RIGHT:
+        print "right key was pressed"
+        starSprite.x += 5
+        global lookLeft
+        if lookLeft != 0:
+            lookLeft = 0
+            starSprite.image = starRight
+
+    if symbol == key.SPACE:
+        print "space was pressed"
+        """animation dance"""
+        global lookLeft
+        if(lookLeft != 2):
+            lookLeft = 2
+            starSprite.image = danceAnimation
+
+
 
 @window.event()
 def on_mouse_press(x, y, button, modifiers):
@@ -39,7 +77,8 @@ def on_mouse_press(x, y, button, modifiers):
 def on_draw():
     window.clear()
     label.draw()
-    image.blit(0, 0)
+    """image.blit(0, 0)"""
+    starSprite.draw()
 
 
 pyglet.app.run()

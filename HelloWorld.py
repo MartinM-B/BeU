c2b9d866f5d43344aaa1e05ce4b4fa81c479e122
Florian Eckerstorfer <Florian.eckerstorfer@gmx.net>
2014-03-24 11:15:06 +0100

    some basic things

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,17 +1,43 @@
 __author__ = 'win8'
 
 import pyglet
+from pyglet.window import key
+from pyglet.window import mouse
+
 
 print("Be aware that pyglet requires 32bit version of python!")
 
 window = pyglet.window.Window()
 
-label = pyglet.text.Label('Hello world!',font_name='Times New Roman',font_size=36,x=window.width//2, y=window.height//2,anchor_x='center', anchor_y='center')
+label = pyglet.text.Label('Hello world!', font_name='Times New Roman', font_size=36, x=window.width // 2,
+                          y=window.height // 2, anchor_x='center', anchor_y='center')
+
+""" use any picture """
+image  = pyglet.resource.image("small_cat.jpg")
+
+@window.event()
+def on_key_press(symbol, modifiers):
+    print "a key was pressed"
+    if symbol== key.LEFT:
+        print "left key was pressed"
+
+@window.event()
+def on_mouse_press(x, y, button, modifiers):
+    if button == mouse.LEFT:
+        print "left mouse pressed"
+
+    elif button == mouse.RIGHT:
+        print "right mouse pressed"
+
+
+
 
 @window.event
 def on_draw():
     window.clear()
     label.draw()
+    image.blit(0, 0)
+
 
 pyglet.app.run()
 

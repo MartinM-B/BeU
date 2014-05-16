a9cf38c3b223bcf8f77ba2708e913c6ddb33137d
Florian Eckerstorfer <Florian.eckerstorfer@gmx.net>
2014-05-16 12:24:09 +0200

    deleted receiver

--- src/states/StateMachine.py
+++ src/states/StateMachine.py
@@ -1,17 +1,13 @@
 __author__ = 'florian'
 
 from State import *
-from src.Messenger.receiver import *
 
 
-class StateMachine(State, Receiver):
+class StateMachine(State):
 
     __states = {}
 
     def onEnter(self):
-        pass
-
-    def onReceive(self):
         pass
 
     def onExit(self):

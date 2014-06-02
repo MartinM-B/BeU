3b3fd7123aa406faa1a8b0b20d8e7c6e499fe303
David Eibensteiner <david.eibensteiner@catalysts.cc>
2014-06-02 11:49:04 +0200

    Added Viking again to the game, preformance improvements

--- src/InYourFaceGame.py
+++ src/InYourFaceGame.py
@@ -19,9 +19,10 @@ from src.RoundCounter.roundcounter import *
 
 
 pyglet.options['audio'] = ('openal')
+pyglet.options['debug_gl'] = False
 
 # create a simple window
-window = pyglet.window.Window(1024, 600, caption="collision", visible=False) #60x480
+window = pyglet.window.Window(caption="collision", visible=False, fullscreen=True) #60x480
 
 label = pyglet.text.Label('Click to create falling objects (collision is pixel perfect but used image not)!', font_name='Times New Roman', font_size=10, x=window.width // 2,
                           y=4 * window.height // 5, anchor_x='center', anchor_y='center', color=(0, 0, 0, 255))
@@ -31,15 +32,12 @@ batch = pyglet.graphics.Batch()
 background = pyglet.graphics.OrderedGroup(0)
 foreground = pyglet.graphics.OrderedGroup(1)
 
-# load our images
-block = resources.block
-
 # create the level as an entity
 #player = Player(batch, foreground)
 
-player = ChibiUsa(batch, foreground)
+#player = ChibiUsa(batch, foreground)
 #TODO resize Viking (downscale)
-#player = Viking(batch, foreground)
+player = Viking(batch, foreground)
 player2 = ChibiUsa_blue(batch, foreground)
 player2.moveX(200)
 
@@ -49,11 +47,6 @@ roundcounter = RoundCounter(batch, player, player2, 285, 400, 3)
 
 playerOneInputController = PlayerOneKeyboardInputHandler(player)
 playerTwoInputController = PlayerTwoKeyboardInputHandler(player2)
-
-# create a set to contain the blocks
-# a set has a very fast difference operation,
-# which we will use in the update function
-blocks = set()
 
 @window.event()
 def on_key_press(symbol, modifiers):
@@ -71,23 +64,17 @@ def on_key_release(symbol, modifiers):
 def on_mouse_press(x, y, button, modifiers):
         '''Create a new block whenever the user clicks the mouse'''
 
-        # create a new block
-        b = GameEntity(block, x, y, batch, background)
-        # add it to the set
-        blocks.add(b)
-
-
 fps_display = pyglet.clock.ClockDisplay()
 
 @window.event
 def on_draw():
         # clear the window
         window.clear()
-
-        label.draw()
-        fps_display.draw()
         # draw our background and blocks
         batch.draw()
+        label.draw()
+        fps_display.draw()
+
 
         healthbar.draw()
         healthbar2.draw()
@@ -106,18 +93,6 @@ def update(dt):
     if checkEnumValueEquals(player2.actionState, ActionState.Attacking) and player2.checkCollision(player):
         print "Player2 Kollission"
         player.playerHit(checkEnumValueEquals(player2.lookDirection, Direction.Right) and Direction.Left or Direction.Right, player2)
-
-    for b in blocks:
-            # don't let block fall out of the window bounds
-            if b.y < -5:
-                    b.setPosition(b.x, -5)
-
-            # apply gravity
-            b.moveY(-8)
-
-            # move the block back upwards if they collide
-            while b.checkCollision(player):
-                    b.moveY(+1)
 
     update_rounds()
 

8da01d283a344c6eeab94b9f3ed827e81678d5ca
Florian Eckerstorfer <Florian.eckerstorfer@gmx.net>
2014-06-14 18:31:32 +0200

    test added, resources can't be found, for whatever reason

---
+++ src/gui/gui_test.py
@@ -0,0 +1,69 @@
+from src import gui_resources
+
+__author__ = 'florian'
+import pyglet
+from src.gui.PyLayouter import *
+from src.gui_resources import *
+from src.gui.PyPoint import *
+from src.gui.PyClickListener import *
+from src import gui_resources
+from pyglet.gl import *
+
+
+class PyListener(PyClickListener):
+
+    def onClick(self, anID):
+        print "" + anID + "was clicked"
+
+window = pyglet.window.Window(caption="collision", visible=False, fullscreen=True) #60x480
+
+# create the render structures
+batch = pyglet.graphics.Batch()
+background = pyglet.graphics.OrderedGroup(0)
+layout = PyLayouter(window)
+
+button1_res = gui_resources.box
+button2_res = gui_resources.box
+point1 = PyPoint(100, 100)
+point2 = PyPoint(350, 350)
+listener = PyListener()
+
+button1 = PyButton("button1", listener, point1, batch, button1_res, background)
+button2 = PyButton("button1", listener, point2, batch, button2_res, background)
+
+layout.addButton(button1)
+layout.addButton(button2)
+
+@window.event
+def on_draw():
+        # clear the window
+        window.clear()
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+        # draw our background and blocks
+        batch.draw()
+
+window.clear()
+window.flip()
+
+# make the window visible
+window.set_visible(True)
+
+# and finally, run the app...
+pyglet.app.run()
+
+
+@window.event()
+def on_key_press(symbol, modifiers):
+    print "a key was pressed"
+    layout.handleKeyPress(symbol, modifiers)
+
+@window.event()
+def on_key_release(symbol, modifiers):
+    print "a key was released"
+    layout.handleKeyRelease(symbol, modifiers)
+
+
+
